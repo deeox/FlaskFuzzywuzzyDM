d4aedc01450e7d7174b25005f57a236176fbdd6c
deeox <deepakdt159@gmail.com>
2018-05-24 12:15:09 +0530

    Add files via upload

---
+++ flaskFuzzywuzzyDM/master.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, redirect, request, url_for, flash
+from fuzzywuzzy import fuzz
+from doublemetaphone import doublemetaphone as dm
+
+
+def homophone(str1, str2):
+    print(dm(str1))
+    print(dm(str2))
+    if fuzz.ratio(dm(str1), dm(str2)) == 100:
+        return 1
+
+
+app = Flask(__name__)
+
+
+@app.route('/results')
+def correct(val1, val2, flag, fuzzyDM, fuzzyNoDM):
+    return render_template("results.html", value1=val1, value2=val2, ptr=flag, fuzzyDM=fuzzyDM, fuzzyNoDM=fuzzyNoDM)
+
+
+@app.route('/')
+def home():
+    return render_template("homepage.html")
+
+
+@app.route('/', methods=['POST'])
+def homepage():
+    value1 = str(request.form['input1'])
+    value2 = str(request.form['input2'])
+
+    fuzzyNoDM = fuzz.ratio(value1, value2)
+    fuzzyDM = fuzz.ratio(dm(value1)[0], dm(value2)[1])
+    ptr = homophone(value1, value2)
+    print(ptr)
+    redirect('/results')
+
+    return correct(value1, value2, ptr, fuzzyDM, fuzzyNoDM)
+
+
+
+
+
